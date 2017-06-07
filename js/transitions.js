2d657024f40267dca6eff67ab191fb9d2c0c77a4
Cole Boothman <coleboothman2@gmail.com>
2017-06-07 00:14:42 -0700

    bugfix for contact page animations

--- js/transitions.js
+++ js/transitions.js
@@ -4,15 +4,21 @@
 	$("#contact").click(function() {
 		$("#home-page").fadeOut(1000);
 		$("#contact-page").delay(1000).fadeIn(1000);
+		load_contact_page();
 		
+		
+	
+	});
+
+	function load_contact_page() {
 		//here, we want the page to be visible first, followed by the header, then the icons.
 		$("#contact-title").delay(1500).fadeIn(750);
-		
 		$("#linked-in").delay(2500).fadeIn(1000);
 		$("#gmail").delay(2500).fadeIn(1000);
 		$("#github").delay(2500).fadeIn(1000);
 		$("#resume").delay(2500).fadeIn(1000);
-	});
+	}
+
 
 
 	//transition for projects page
@@ -25,9 +31,18 @@
 
 		//check which page we are on to make sure we set the display:none for the proper page.
 		if($('#contact-page').is(':visible')){	
-			$("#contact-page").fadeOut(1000);
-			$("#home-page").delay(1000).fadeIn(1000);
-			console.log("contract page");
+			
+			//animate fade of header and icons first before page
+			$("#contact-title").fadeOut(750);
+			$("#linked-in").delay(1000).fadeOut(1000);
+			$("#gmail").delay(1000).fadeOut(1000);
+			$("#github").delay(1000).fadeOut(1000);
+			$("#resume").delay(1000).fadeOut(1000);	
+
+			$("#contact-page").delay(1000).fadeOut(1000);
+			$("#home-page").delay(2000).fadeIn(1000);
+			
+			
 			
 		}
 
