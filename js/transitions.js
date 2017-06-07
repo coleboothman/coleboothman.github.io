7ecfaf52cde5bce4540c7b41de38c1762af25ad5
Cole Boothman <coleboothman2@gmail.com>
2017-06-06 22:04:32 -0700

    added bouncing button animation for proj/contact pages

--- js/transitions.js
+++ js/transitions.js
@@ -1,8 +1,35 @@
 (function() {
 	
-	//transition for contacts
+	//transition for contacts page
 	$("#contact").click(function() {
 		$("#home-page").fadeOut(1000);
+		$("#contact-page").fadeIn(1000);
+	});
+
+
+	//transition for projects page
+	$("#proj").click(function(){
+		$("#home-page").fadeOut(1000);
+		$("#project-page").fadeIn(1000);
+	});
+
+	$(".img-bounce").click(function() {
+
+		//check which page we are on to make sure we set the display:none for the proper page.
+		if($('#contact-page').is(':visible')){	
+			$("#contact-page").fadeOut(1000);
+			$("#home-page").fadeIn(1000);
+			console.log("contract page");
+			
+		}
+
+		if($("#project-page").is(':visible')) {
+			$("#project-page").fadeOut(1000);
+			$("#home-page").fadeIn(1000);
+			console.log("project page");
+		}
+
+	
 	});
 
 
